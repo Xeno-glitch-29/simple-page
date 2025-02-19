0f324f91408fde80ae9f1ab8aaea18fe39ec6097
adam adam <am29gamer@gmail.com>
2025-02-19 15:05:14 +0100

    create js file and  first logic for the website

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("hi im adam");
+
+let aboutButton = "ula"
+console.log("im: "  + aboutButton);
+
+let button = document.querySelector(" .about .btn")
+console.log(button);
+
+button.addEventListener("click",()=> {
+    alert("i have created an event")
+})
