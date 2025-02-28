43b8812d69e223409bee8bd1fa5b9ab4dc12f274
adam adam <am29gamer@gmail.com>
2025-02-28 15:58:58 +0100

    js data types learning

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-console.log("hi im adam");
+// console.log("hi im adam");
 
-let aboutButton = "ula"
-console.log("im: "  + aboutButton);
+// let aboutButton = "ula"
+// console.log("im: "  + aboutButton);
 
-let button = document.querySelector(" .about .btn")
-console.log(button);
+// let button = document.querySelector(" .about .btn")
+// console.log(button);
 
-button.addEventListener("click",()=> {
-    alert("i have created an event")
-})
+// button.addEventListener("click",()=> {
+//     alert("i have created an event")
+// })
+
+let myName = "adam"
+let number = 10
+let array = [100, 12, 28, 68,]
+let object = {
+    car: "audi",
+    production: "last year",
+    colour:"black"
+}
+console.log(myName);
+console.log(number);
+console.log(array);
+console.log(object);
+console.log("we are getting the conreate value from the array" + array[2]);
+console.log(object.colour);
+myName = "ula"
+console.log(myName);
